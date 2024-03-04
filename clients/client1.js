aef1a0275fbf24de184d95e4524f63b6e072b379
Petar Hajduk <hajduk.petar@gmail.com>
2024-03-04 18:39:30 +0100

    client

---
+++ clients/client1.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://localhost:3000?id=12345678');
+
+ws.on('open', () => {
+  console.log('Connected to WebSocket server');
+});
+
+ws.on('message', (data) => {
+  console.log('Received message:', data);
+});
+
+ws.on('close', () => {
+  console.log('Disconnected from WebSocket server');
+});
+
+ws.on('error', (error) => {
+  console.error('WebSocket error:', error);
+});
