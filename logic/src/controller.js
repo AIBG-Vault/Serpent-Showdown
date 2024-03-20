b7c314e401ab6babd5bc9c8076a93ad077eebd25
OffCrazyFreak <jakovbla4@gmail.com>
2024-03-20 12:07:18 +0100

    Moved visuals to visuals folder. Added creating new game on controller boot

--- logic/src/controller.js
+++ logic/src/controller.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
-const bodyParser = require('body-parser');
-const { GameField } = require('./gameFiles/field');
-const { IllegalMoveException} = require('./gameFiles/util/illegalMoveException');
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const bodyParser = require("body-parser");
+const { GameField } = require("./gameFiles/field");
+const {
+  IllegalMoveException,
+} = require("./gameFiles/util/illegalMoveException");
 
 const app = express();
 const port = 3000;
@@ -15,18 +17,17 @@ let waitingForOtherPlayer = false;
 let currentTurn = 0;
 
 const players = [
-    
-    {
-        name: "Player 1",
-        id: "1234567890"
-    },
-    {
-        name: "Player 2",
-        id: "0987654321"
-    }
+  {
+    name: "Player 1",
+    id: "1234567890",
+  },
+  {
+    name: "Player 2",
+    id: "0987654321",
+  },
 ];
 
-let gameObject;
+let gameObject = new GameField(players);
 
 // Create an HTTP server
 const server = http.createServer(app);
@@ -37,191 +38,178 @@ const wss = new WebSocket.Server({ server });
 // WebSocket connections
 const connections = new Set();
 
-wss.on('connection', (ws, req) => {
+wss.on("connection", (ws, req) => {
+  const url = new URL(req.url, "http://localhost:3000"); // Adjust the origin as needed
+  const receivedId = url.searchParams.get("id");
+  ws.id = receivedId;
+  connections.add(ws);
 
-    const url = new URL(req.url, 'http://localhost:3000'); // Adjust the origin as needed
-    const receivedId = url.searchParams.get('id');
-    ws.id = receivedId;
-    connections.add(ws);
+  if (receivedId === "frontend") {
+    console.log("Frontend connected");
 
-    if (receivedId === 'frontend') {
-        console.log('Frontend connected');
+    ws.send(
+      JSON.stringify({
+        field: gameObject.field,
+        player1: players[0].name,
+        player2: players[1].name,
+        winner: gameObject.winner,
+      })
+    );
+  } else if (receivedId === players[0].id || receivedId === players[1].id) {
+    connectedPlayers++;
 
-        ws.send(JSON.stringify({
+    console.log("Connections:", connections.size);
 
+    if (connectedPlayers === 2) {
+      waitingForOtherPlayer = false;
+      gameObject = new GameField(players);
+
+      // Send the game state to the connected players
+      connections.forEach((client) => {
+        if (
+          client.readyState === WebSocket.OPEN &&
+          client.id === players[0].id
+        ) {
+          const message = JSON.stringify({
             field: gameObject.field,
-            player1: players[0].name,
-            player2: players[1].name,
-            winner: gameObject.winner
+            currentTurn: players[gameObject.turn].name,
+            winner: gameObject.winner,
+          });
 
-        }));
+          client.send(message, (error) => {
+            if (error) {
+              console.error("Error sending message:", error);
+            }
+          });
+        }
+      });
+    } else {
+      ws.send(
+        JSON.stringify({
+          message:
+            "Player registered successfully. Waiting for the other player.",
+        })
+      );
+      waitingForOtherPlayer = true;
+    }
+  } else {
+    ws.send(
+      JSON.stringify({
+        message: "Invalid ID. Connection rejected.",
+        id: receivedId,
+      })
+    );
+    ws.close();
+    console.log("Connection rejected. Invalid ID:", receivedId);
+  }
 
-    } else if (receivedId === players[0].id || receivedId === players[1].id) {
+  ws.on("message", (message) => {
+    console.log(`Received message: ${message}`);
 
-        connectedPlayers++;
+    if (waitingForOtherPlayer) {
+      ws.send(
+        JSON.stringify({ message: "Waiting for the other player to connect." })
+      );
+      return;
+    } else {
+      let move;
 
-        console.log('Connections:', connections.size);
+      try {
+        move = JSON.parse(message);
+      } catch (error) {
+        console.error("Cannot parse message:", message);
+        return;
+      }
 
-        if (connectedPlayers === 2) {
+      console.log(`Received move: ${move}`);
 
-            waitingForOtherPlayer = false;
-            gameObject = new GameField(players)
+      //console.log('AAA', move.playerId === players[currentTurn].id)
+      if (move.playerId === players[currentTurn].id) {
+        try {
+          gameObject.playMove(move);
+        } catch (error) {
+          if (error instanceof IllegalMoveException) {
+            ws.send(
+              JSON.stringify({
+                message:
+                  "Illegal move. Please try another move or restart the game by disconnecting both agents and connecting them again.",
+                code: 100,
+                move: move,
+              })
+            );
+            return;
+          } else {
+            throw error;
+          }
+        }
 
-            // Send the game state to the connected players
-            connections.forEach((client) => {
+        // if (gameObject.winner === 0 || gameObject.winner === 1) {
+        //     wss.close();
+        // }
 
-                if (client.readyState === WebSocket.OPEN && client.id === players[0].id) {
+        currentTurn = gameObject.turn;
 
-                    const message = JSON.stringify({
-
-                        field: gameObject.field,
-                        currentTurn: players[gameObject.turn].name,
-                        winner: gameObject.winner
-
-                    });
-
-                    client.send(message, (error) => {
-
-                        if (error) {
-                            console.error('Error sending message:', error);
-                        }
-                        
-                    });
-                }
+        connections.forEach((client) => {
+          if (
+            (client.readyState === WebSocket.OPEN &&
+              client.id === players[currentTurn].id) ||
+            (client.readyState === WebSocket.OPEN &&
+              (gameObject.winner === 0 || gameObject.winner === 1))
+          ) {
+            const message = JSON.stringify({
+              field: gameObject.field,
+              currentTurn: players[gameObject.turn].name,
+              winner: gameObject.winner,
             });
 
-        } else {
+            client.send(message, (error) => {
+              if (error) {
+                console.error("Error sending message:", error);
+              }
+            });
+          } else if (
+            client.readyState === WebSocket.OPEN &&
+            client.id === "frontend"
+          ) {
+            const message = JSON.stringify({
+              field: gameObject.field,
+              player1: players[0].name,
+              player2: players[1].name,
+              winner: gameObject.winner,
+            });
 
-            ws.send(JSON.stringify({ message: 'Player registered successfully. Waiting for the other player.' }))
-            waitingForOtherPlayer = true;
+            client.send(message, (error) => {
+              if (error) {
+                console.error("Error sending message:", error);
+              }
+            });
+          }
+        });
+      } else {
+        connections.forEach((client) => {
+          const message = JSON.stringify({
+            message:
+              "It is not your turn. Please wait for the other player to make a move.",
+          });
 
-        }
-    } else {
-        ws.send(JSON.stringify({
-            message: 'Invalid ID. Connection rejected.',
-            id: receivedId
-        }));
-        ws.close();
-        console.log('Connection rejected. Invalid ID:', receivedId);
+          client.send(message, (error) => {
+            if (error) {
+              console.error("Error sending message:", error);
+            }
+          });
+        });
+      }
     }
+  });
 
-    ws.on('message', (message) => {
+  ws.on("close", () => {
+    // Remove the closed connection from the set
+    connections.delete(ws);
+    //console.log('Connections:', connections.size);
+    connectedPlayers--;
 
-        console.log(`Received message: ${message}`);
-
-        if (waitingForOtherPlayer) {
-
-            ws.send(JSON.stringify({ message: 'Waiting for the other player to connect.' }));
-            return;
-
-        } else {
-
-            let move;
-
-            try {
-                move = JSON.parse(message);
-            } catch (error) {
-                console.error('Cannot parse message:', message);
-                return;
-            }
-
-            console.log(`Received move: ${move}`);
-
-            //console.log('AAA', move.playerId === players[currentTurn].id)
-            if (move.playerId === players[currentTurn].id) {
-
-                try {
-                    gameObject.playMove(move);
-                } catch (error) {
-                    if (error instanceof IllegalMoveException) {
-                        ws.send(JSON.stringify({ 
-                            message: 'Illegal move. Please try another move or restart the game by disconnecting both agents and connecting them again.',
-                            code: 100,
-                            move: move
-                        }));
-                        return;
-                    } else {
-                        throw error;
-                    }
-                }
-
-                // if (gameObject.winner === 0 || gameObject.winner === 1) {
-                //     wss.close();
-                // }
-
-                currentTurn = gameObject.turn;
-
-                connections.forEach((client) => {
-
-                    if ((client.readyState === WebSocket.OPEN && client.id === players[currentTurn].id) ||
-                        (client.readyState === WebSocket.OPEN && (gameObject.winner === 0 || gameObject.winner === 1))) {
-
-                        const message = JSON.stringify({
-
-                            field: gameObject.field,
-                            currentTurn: players[gameObject.turn].name,
-                            winner: gameObject.winner
-
-                        });
-
-                        client.send(message, (error) => {
-
-                            if (error) {
-                                console.error('Error sending message:', error);
-                            }
-
-                        });
-                    } else if (client.readyState === WebSocket.OPEN && client.id === 'frontend') {
-
-                        const message = JSON.stringify({
-
-                            field: gameObject.field,
-                            player1: players[0].name,
-                            player2: players[1].name,
-                            winner: gameObject.winner
-
-                        });
-
-                        client.send(message, (error) => {
-
-                            if (error) {
-                                console.error('Error sending message:', error);
-                            }
-
-                        });
-
-                    }
-                });
-            } else {
-                connections.forEach((client) => {
-                    const message = JSON.stringify({
-
-                        message: 'It is not your turn. Please wait for the other player to make a move.'
-
-                    });
-
-                    client.send(message, (error) => {
-
-                        if (error) {
-                            console.error('Error sending message:', error);
-                        }
-
-                    });
-                });
-            }
-        }
-
-    });
-
-    ws.on('close', () => {
-        // Remove the closed connection from the set
-        connections.delete(ws);
-        //console.log('Connections:', connections.size);
-        connectedPlayers--;
-
-        gameObject = new GameField(players);
-    });
-
+    gameObject = new GameField(players);
+  });
 });
 
 // Endpoint for user registration and starting the game
@@ -277,5 +265,5 @@ wss.on('connection', (ws, req) => {
 
 // Start the server
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
