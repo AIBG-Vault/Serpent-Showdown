3f97acfdc4a55a7012144949dc1cce1130539c3d
OffCrazyFreak <jakovbla4@gmail.com>
2025-05-28 19:34:57 +0200

    Reverted changes, refractored code

--- visuals/js/board.js
+++ visuals/js/board.js
@@ -49,37 +49,23 @@ function updateGrid(data) {
         if (typeof value === "object" && value !== null) {
           cell.classList.add(value.type || "unknown");
 
-          // Handle object values
-          if (value.type === "snake-head") {
+          if (value.type.includes("snake")) {
             cell.classList.add("snake");
-            cell.classList.add("snake-head");
             cell.classList.add(
-              value.playerName === data.players[0].name
-                ? "snake-player1-head"
-                : "snake-player2-head"
+              value.playerName === data.players[0].name ? "player1" : "player2"
             );
 
-            // add img to cell
-            const img = document.createElement("img");
-            img.src = "./img/sprites/AIBG 6.0 Zagreb bear.png";
-            img.alt = "snake head";
-            cell.appendChild(img);
-          } else if (value.type === "snake-body") {
-            cell.classList.add("snake");
-            cell.classList.add("snake-body");
-            cell.classList.add(
-              value.playerName === data.players[0].name
-                ? "snake-player1-body"
-                : "snake-player2-body"
-            );
+            if (value.type === "snake-head") {
+              cell.classList.add("snake-head");
 
-            // add img to cell
-            const img = document.createElement("img");
-            img.src = `./img/sprites/snake/tijelo_${
-              value.playerName === data.players[0].name ? "n" : "p"
-            }_25.png`;
-            img.alt = "snake body";
-            cell.appendChild(img);
+              // add img to cell
+              const img = document.createElement("img");
+              img.src = "./img/sprites/snake/AIBG 6.0 Zagreb bear.png";
+              img.alt = "snake head";
+              cell.appendChild(img);
+            } else if (value.type === "snake-body") {
+              cell.classList.add("snake-body");
+            }
           } else {
             cell.classList.add("item");
             // cell.textContent = value.symbol || "?";
@@ -98,189 +84,71 @@ function updateGrid(data) {
     }
   }
 
-  updateSnakesRotations(data.players);
+  // update snake heads rotation
+  updateSnakesHeadRotations(data.players);
 
+  // grayscale snakes on death
   if (data.winner) {
-    if (data.winner === -1) {
-      // draw - grayscale all snakes
-      const allSnakesCells = document.querySelectorAll(".snake");
-      allSnakesCells.forEach((cell) => {
-        cell.style.filter = "grayscale(100%)";
-      });
-    } else {
-      // grayscale the losing snake
-      const losingPlayer = data.players.find(
-        (player) => player.name !== data.winner
-      );
-
-      // find index of losing player
-      const losingPlayerIndex = data.players.findIndex(
-        (player) => player.name === losingPlayer.name
-      );
-
-      const losingSnakeHeadCell = document.querySelector(
-        `.snake-player${losingPlayerIndex + 1}-head`
-      );
-      losingSnakeHeadCell.style.filter = "grayscale(100%)"; // bcs of border
-
-      const losingSnakeBodyCells = document.querySelectorAll(
-        `.snake-player${losingPlayerIndex + 1}-body`
-      );
-      losingSnakeBodyCells.forEach((cell) => {
-        // get img from cell
-        const losingSnakeBodyImg = cell.querySelector("img");
-        losingSnakeBodyImg.style.filter = "grayscale(100%)";
-      });
-    }
+    grayscaleSnakes(data);
   }
 }
 
-function updateSnakesRotations(players) {
-  // rotate snake head, body and tail based on direction
-
+function updateSnakesHeadRotations(players) {
   players.forEach((player, index) => {
-    for (
-      let bodySegmentIndex = 0;
-      bodySegmentIndex < player.body.length;
-      bodySegmentIndex++
-    ) {
-      const previousSegment = player.body[bodySegmentIndex - 1];
-      const currentSegment = player.body[bodySegmentIndex];
-      const followingSegment = player.body[bodySegmentIndex + 1];
+    const playerHead = player.body[0];
+    const playerNeck = player.body[1];
 
-      // console.log(
-      //   `player ${player.name} segment ${bodySegmentIndex} previous`,
-      //   previousSegment
-      // );
-      // console.log(
-      //   `player ${player.name} segment ${bodySegmentIndex}`,
-      //   currentSegment
-      // );
-      // console.log(
-      //   `player ${player.name} segment ${bodySegmentIndex} following`,
-      //   followingSegment
-      // );
+    const snakeHeadElem = document.querySelector(
+      `.snake.snake-head.player${index + 1}`
+    );
 
-      // const previousSegmentElem = document.getElementById(`cell-${previousSegment.row}-${previousSegment.column}`)
-      const currentSegmentElem = document.getElementById(
-        `cell-${currentSegment.row}-${currentSegment.column}`
-      );
-      // const followingSegmentElem = document.getElementById(`cell-${followingSegment.row}-${followingSegment.column}`)
+    if (!playerHead || !playerNeck) {
+      // No head or neck found, reset rotation
+      snakeHeadElem.style.transform = "rotate(0deg)";
+      return;
+    }
 
-      if (!currentSegmentElem) {
-        continue;
-      }
-
-      // get img from cell
-      const snakeImgElem = currentSegmentElem.querySelector("img");
-
-      if (!snakeImgElem) {
-        continue;
-      }
-
-      if (!previousSegment && !followingSegment) {
-        // just head = reset rotation
-        snakeImgElem.style.transform = "rotate(0deg)";
-        continue;
-      }
-
-      // head
-      if (!previousSegment) {
-        if (currentSegment.column > followingSegment.column) {
-          // direction = "left";
-          snakeImgElem.style.transform = "rotate(-90deg)";
-        } else if (currentSegment.column < followingSegment.column) {
-          // direction = "right";
-          snakeImgElem.style.transform = "rotate(90deg)";
-        } else if (currentSegment.row < followingSegment.row) {
-          // direction = "down";
-          snakeImgElem.style.transform = "rotate(180deg)";
-        } else if (currentSegment.row > followingSegment.row) {
-          // direction = "up";
-          snakeImgElem.style.transform = "rotate(0deg)";
-        }
-
-        continue;
-      }
-
-      // tail
-      if (!followingSegment) {
-        snakeImgElem.alt = "snake tail";
-
-        if (index === 0) {
-          snakeImgElem.src = "./img/sprites/snake/rep_n_25.png";
-        } else {
-          snakeImgElem.src = "./img/sprites/snake/rep_p_25.png";
-        }
-        if (previousSegment.column > currentSegment.column) {
-          // direction = "left";
-          snakeImgElem.style.transform = "rotate(90deg)";
-        } else if (previousSegment.column < currentSegment.column) {
-          // direction = "right";
-          snakeImgElem.style.transform = "rotate(-90deg)";
-        } else if (previousSegment.row < currentSegment.row) {
-          // direction = "down";
-          snakeImgElem.style.transform = "rotate(0deg)";
-        } else if (previousSegment.row > currentSegment.row) {
-          // direction = "up";
-          snakeImgElem.style.transform = "rotate(180deg)";
-        }
-
-        continue;
-      }
-
-      // body - rotate diagionally as well
-      if (
-        previousSegment.row < followingSegment.row &&
-        previousSegment.column < followingSegment.column
-      ) {
-        // direction = "up-left";
-        snakeImgElem.style.transform = "rotate(-45deg)";
-      } else if (
-        previousSegment.row < followingSegment.row &&
-        previousSegment.column > followingSegment.column
-      ) {
-        // direction = "up-right";
-        snakeImgElem.style.transform = "rotate(45deg)";
-      } else if (
-        previousSegment.row > followingSegment.row &&
-        previousSegment.column < followingSegment.column
-      ) {
-        // direction = "down-left";
-        snakeImgElem.style.transform = "rotate(-135deg)";
-      } else if (
-        previousSegment.row > followingSegment.row &&
-        previousSegment.column > followingSegment.column
-      ) {
-        // direction = "down-right";
-        snakeImgElem.style.transform = "rotate(135deg)";
-      } else if (
-        previousSegment.row === followingSegment.row &&
-        previousSegment.column < followingSegment.column
-      ) {
-        // direction = "left";
-        snakeImgElem.style.transform = "rotate(-90deg)";
-      } else if (
-        previousSegment.row === followingSegment.row &&
-        previousSegment.column > followingSegment.column
-      ) {
-        // direction = "right";
-        snakeImgElem.style.transform = "rotate(90deg)";
-      } else if (
-        previousSegment.row < followingSegment.row &&
-        previousSegment.column === followingSegment.column
-      ) {
-        // direction = "up";
-        snakeImgElem.style.transform = "rotate(0deg)";
-      } else if (
-        previousSegment.row > followingSegment.row &&
-        previousSegment.column === followingSegment.column
-      ) {
-        // direction = "down";
-        snakeImgElem.style.transform = "rotate(180deg)";
-      }
+    if (playerHead.column > playerNeck.column) {
+      // direction = "left";
+      snakeHeadElem.style.transform = "rotate(-90deg)";
+    } else if (playerHead.column < playerNeck.column) {
+      // direction = "right";
+      snakeHeadElem.style.transform = "rotate(90deg)";
+    } else if (playerHead.row < playerNeck.row) {
+      // direction = "down";
+      snakeHeadElem.style.transform = "rotate(180deg)";
+    } else if (playerHead.row > playerNeck.row) {
+      // direction = "up";
+      snakeHeadElem.style.transform = "rotate(0deg)";
     }
   });
+}
+
+function grayscaleSnakes(data) {
+  if (data.winner === -1) {
+    // draw - grayscale all snakes
+    const allSnakesCells = document.querySelectorAll(".snake");
+    allSnakesCells.forEach((cell) => {
+      cell.style.filter = "grayscale(100%)";
+    });
+  } else {
+    // grayscale the losing snake
+    const losingPlayer = data.players.find(
+      (player) => player.name !== data.winner
+    );
+
+    // find index of losing player
+    const losingPlayerIndex = data.players.findIndex(
+      (player) => player.name === losingPlayer.name
+    );
+
+    const losingSnakeCells = document.querySelectorAll(
+      `.snake.player${losingPlayerIndex + 1}`
+    );
+    losingSnakeCells.forEach((cell) => {
+      cell.style.filter = "grayscale(100%)";
+    });
+  }
 }
 
 // Export functions for use in other files
